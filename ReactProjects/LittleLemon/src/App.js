c6b13a22b5a80f36422f16e5c3c0b6fb4b569411
Riccardo <riccardo.lamalfa94@gmail.com>
2023-02-22 12:07:29 +0100

    Update app.js

--- ReactProjects/LittleLemon/src/App.js
+++ ReactProjects/LittleLemon/src/App.js
@@ -1,10 +1,44 @@
 import './App.css';
+import './Header';
+import './Main';
+import './Footer';
+<>
+<header>
+  <meta title='Little Lemon Restaurant'
+  description='Mediterranean restaurant in Chicago'
+  language='english'
+  author='Riccardo'
+  charset="utf-8"
+  http-equiv='text/html'
+  http-equiv="refresh" content="300"
+  name="format-detection" content="telephone=yes"
+  HandheldFriendly="true"
+  viewport="width=device-width, initial-scale=1.0"
+  />
+</header>
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+<main>
+    <nav>
+        <img src="/assets/Logo.svg" name="Restaurant's logo" />
+        <ul>
+          <li><a href='/about'>About</a></li>;
+          <li>Menu</li>;
+          <li>Reservations</li>;
+          <li>Order Online</li>;
+          <li>Login</li>;
+        </ul>
+    </nav>
+</main>
+
+<footer>
+
+</footer>
+</>
+function App()
+return (
+<className />= <"App"/>
+);
+
 
 export default App;
+
